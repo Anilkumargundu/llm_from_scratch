e898456179522d721a844f232304567792798942
root <root@LAPTOP-80FGD2MK>
2025-04-06 02:48:20 +0800

    input_target_pairs_version2

--- code/input_target_pairs.py
+++ code/input_target_pairs.py
@@ -17,4 +17,5 @@ for i in range (1, context_size+1):
     desired = enc_sample[i]
     #print("context or present input is :", context, "------> predicted output:", desired)
     print(tokenizer.decode(context), "-------->", "------->", tokenizer.decode([desired]))
-    
+
+    #
